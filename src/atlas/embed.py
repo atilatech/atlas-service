63a70bd0a78addf1e4a0ff5dc71e53c88c729f09
ademidun <tomiademidun@gmail.com>
2022-12-31 21:28:09 -0500

    update service without sentence transformer

--- src/atlas/embed.py
+++ src/atlas/embed.py
@@ -1,10 +1,11 @@
-from sentence_transformers import SentenceTransformer
+import json
+
 import pinecone
+import requests
 from tqdm import tqdm
 
 from atlas.utils import parse_video_id
-from config import PINECONE_API_KEY
-from functools import lru_cache
+from config import PINECONE_API_KEY, HUGGING_FACE_ENDPOINT_URL, HUGGING_FACE_API_KEY
 
 sentence_transformer_model_model_id = "multi-qa-mpnet-base-dot-v1"
 pinecone_index_id = "youtube-search"
@@ -18,13 +19,22 @@ pinecone.init(
 pinecone_index = pinecone.Index(pinecone_index_id)
 
 
-@lru_cache
-def get_sentence_transformer_model(model_id: str = sentence_transformer_model_model_id) -> SentenceTransformer:
-    return SentenceTransformer(model_id)
+def send_encoding_request(phrase: str):
+    payload = json.dumps({
+        "inputs": "",  # inputs key is not used but our endpoint expects it
+        "segments": [{'text': phrase, 'id': '', }],
+    })
+    headers = {
+        'Authorization': f'Bearer {HUGGING_FACE_API_KEY}',
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", HUGGING_FACE_ENDPOINT_URL, headers=headers, data=payload)
+    return response.json()
 
 
 def initialize_pinecone_index():
-    dimensions = get_sentence_transformer_model().get_sentence_embedding_dimension()
+    dimensions = 768
     if pinecone_index_id not in pinecone.list_indexes():
         pinecone.create_index(
             pinecone_index_id,
@@ -57,27 +67,26 @@ def upload_transcripts_to_vector_db(transcripts_for_upload):
         batch_meta = [{
             **transcripts_for_upload[x]
         } for x in range(i, i_end)]
-        # extract only text to be encoded by embedding model
-        batch_text = [
-            row['text'] for row in transcripts_for_upload[i:i_end]
-        ]
         # create the embedding vectors
-        batch_embeds = get_sentence_transformer_model().encode(batch_text).tolist()
+        batch_vectors = [
+            row['vectors'] for row in transcripts_for_upload[i:i_end]
+        ]
         # extract IDs to be attached to each embedding and metadata
         batch_ids = [
             row['id'] for row in transcripts_for_upload[i:i_end]
         ]
         # 'upsert' (insert) IDs, embeddings, and metadata to index
         to_upsert = list(zip(
-            batch_ids, batch_embeds, batch_meta
+            batch_ids, batch_vectors, batch_meta
         ))
         pinecone_index.upsert(to_upsert)
         print(f'Uploaded Batches: {i} to {i_end}')
 
 
 def query_model(query, video_id=""):
-    encoded_query = get_sentence_transformer_model().encode(query).tolist()
+    encoded_query = send_encoding_request(query)
     metadata_filter = {"video_id": {"$eq": video_id}} if video_id else None
-    return pinecone_index.query(encoded_query, top_k=5,
+    vectors = encoded_query[0]['vectors']
+    return pinecone_index.query(vectors, top_k=5,
                                 include_metadata=True,
                                 filter=metadata_filter).to_dict()
--- src/atlas/transcribe.py
+++ src/atlas/transcribe.py
@@ -2,7 +2,6 @@ import json
 import time
 
 import requests
-from tqdm import tqdm
 
 from atlas.embed import upload_transcripts_to_vector_db, query_model, does_video_exist
 from atlas.utils import convert_seconds_to_string
@@ -11,7 +10,8 @@ from config import HUGGING_FACE_API_KEY, HUGGING_FACE_ENDPOINT_URL
 
 def send_transcription_request(url: str):
     payload = json.dumps({
-        "inputs": url
+        "inputs": "",  # inputs key is not used but our endpoint expects it
+        "video_url": url,
     })
     headers = {
         'Authorization': f'Bearer {HUGGING_FACE_API_KEY}',
@@ -22,43 +22,11 @@ def send_transcription_request(url: str):
     return response.json()
 
 
-def combine_transcripts(video):
-    new_transcript_segments = []
-    window = 6  # number of sentences to combine
-    stride = 3  # number of sentences to 'stride' over, used to create overlap
-
-    video_info = video['video']
-    transcript_segments = video['transcript']['segments']
-    for i in tqdm(range(0, len(transcript_segments), stride)):
-        i_end = min(len(transcript_segments) - 1, i + window)
-        text = ' '.join(transcript['text']
-                        for transcript in
-                        transcript_segments[i:i_end])
-        # TODO: Should int (float to seconds) conversion happen at the API level?
-        start = int(transcript_segments[i]['start'])
-        end = int(transcript_segments[i]['end'])
-        new_transcript_segments.append({
-            **video_info,
-            **{
-                'start': start,
-                'end': end,
-                'title': video_info['title'],
-                'text': text,
-                'id': f"{video_info['id']}-t{start}",
-                'url': f"https://youtu.be/{video_info['id']}?t={start}",
-                'video_id': video_info['id'],
-            }
-        })
-    return new_transcript_segments
-
-
 def transcribe_and_search_video(url, query, verbose=True):
     t0 = time.time()
     if not does_video_exist(url):
         video_with_transcript = send_transcription_request(url)
-        video_with_transcript_combined = combine_transcripts(video_with_transcript)
-
-        upload_transcripts_to_vector_db(video_with_transcript_combined)
+        upload_transcripts_to_vector_db(video_with_transcript['encoded_segments'])
     else:
         print(f'Skipping transcribing and embedding.'
               ' Video already exists:{url}')
